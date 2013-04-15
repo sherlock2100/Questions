1b2a11c0ebdcc596900b2bf8b01c06b92d426ca5
Ryan Sepassi <rsepassi@gmail.com>
2013-04-15 10:03:02 -0700

    Fleshed out votes controller

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,4 +1,11 @@
 class VotesController < ApplicationController
   def create
+    @vote = current_user.votes.new(params[:vote])
+
+    if @vote.save
+      render json: @vote
+    else
+      render :nothing, status: :unprocessable_entity
+    end
   end
 end
