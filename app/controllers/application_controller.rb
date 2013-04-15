810dc3c8e3c0c0273827b67cbe44c4975ab32695
Ryan Sepassi <rsepassi@gmail.com>
2013-04-15 10:08:10 -0700

    Set up login and session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,24 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :check_logged_in
+
+  def current_user
+    @current_user ||= User.find_by_id(session[:user_id])
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def login(user)
+    @current_user = user
+    session[:user_id] = user.id
+  end
+
+  def check_logged_in
+    unless logged_in?
+      render json: { 'error': 'Must be logged in' }, status: :forbidden
+    end
+  end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,8 @@
 class SessionsController < ApplicationController
   def create
+    @user = User.find(params[:session][:user_id])
+    login(@user)
+
+    render :nothing, status: :ok
   end
 end
